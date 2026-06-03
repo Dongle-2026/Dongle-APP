944aa4a8d6bba48acb14905539d5dc725a924ce2
soyuniii <pe1229@naver.com>
2026-06-03 11:47:50 +0900

    chore: nativewind 설정 적용

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,13 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
-  return <View></View>;
+  return (
+    <View className="flex-1 bg-red-500 items-center justify-center">
+      <Text className="text-point">NativeWind 테스트</Text>
+
+      <View className="mt-4 bg-point px-4 py-2 rounded-lg">
+        <Text className="text-mono-100">버튼</Text>
+      </View>
+    </View>
+  );
 }
