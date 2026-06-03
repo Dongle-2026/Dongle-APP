43c3f02fc989f4676f24e74c34fc28b4c1d05384
soyuniii <pe1229@naver.com>
2026-06-03 11:59:06 +0900

    chore: nativewind 핫리로드 호환성 패치 설정

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 bg-red-500 items-center justify-center">
+    <View className="flex-1 bg-mono-100 items-center justify-center">
       <Text className="text-point">NativeWind 테스트</Text>
 
       <View className="mt-4 bg-point px-4 py-2 rounded-lg">
