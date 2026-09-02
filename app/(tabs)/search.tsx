b93c9ea19675b9ccb360b044f8813ce70bf53e28
soyuniii <pe1229@naver.com>
2026-09-02 13:52:31 +0900

    feat: 검색화면 초기 디자인 구현

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,5 +1,53 @@
-import { View } from 'react-native';
-
+import CategorySlider from '@/components/CategorySlider';
+import { categories, keywordNews } from '@/utils/mock';
+import { Search } from 'lucide-react-native';
+import { useState } from 'react';
+import { FlatList, Image, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 export default function SearchScreen() {
-  return <View></View>;
+  const [selectedCategory, setSelectedCategory] = useState('all');
+  return (
+    <SafeAreaView className="bg-mono-100">
+      <View className="mx-4 h-14 flex-row items-center rounded-2xl border-[2px] border-mono-400/30 bg-white/70 px-4">
+        <Search size={20} color="#737373" />
+
+        <TextInput
+          placeholder="지금 주목할 소식"
+          placeholderTextColor="#A3A3A3"
+          className="ml-3 flex-1 text-[15px]"
+        />
+      </View>
+
+      <CategorySlider
+        category={categories}
+        selectedCategory={selectedCategory}
+        onPress={(categoryId) => setSelectedCategory(categoryId)}
+      />
+
+      <FlatList
+        data={keywordNews}
+        numColumns={3}
+        scrollEnabled={false}
+        keyExtractor={(item) => item.id}
+        columnWrapperStyle={{ gap: 2 }}
+        contentContainerStyle={{ gap: 2 }}
+        className="px-4"
+        renderItem={({ item }) => (
+          <TouchableOpacity className="flex-1 h-48 overflow-hidden ">
+            <Image
+              source={{ uri: item.image }}
+              className="absolute inset-0 h-full w-full"
+              resizeMode="cover"
+            />
+            <View className="absolute inset-0" style={{ backgroundColor: 'rgba(0,0,0,0.4)' }} />
+            <View className="flex-1 justify-center p-3">
+              <Text numberOfLines={2} className="text-xl font-bold text-white text-center">
+                {item.keyword}
+              </Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
+    </SafeAreaView>
+  );
 }
