56e0e2546acf76ee9a69256645abdcf09d5ab584
soyuniii <pe1229@naver.com>
2026-09-07 12:19:04 +0900

    feat: 헤더 애니메이션 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Link, Tabs } from 'expo-router';
 import { SymbolView } from 'expo-symbols';
-import { Image, Pressable } from 'react-native';
+import { Pressable } from 'react-native';
 
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
 import { useColorScheme } from '@/components/useColorScheme';
@@ -16,19 +16,13 @@ export default function TabLayout() {
         // Disable the static render of the header on web
         // to prevent a hydration error in React Navigation v6.
         headerShown: useClientOnlyValue(false, true),
-        headerTitle: () => (
-          <Image
-            source={require('@/assets/images/logo1.png')}
-            style={{ width: 75, height: 20 }}
-            resizeMode="contain"
-          />
-        ),
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: '',
+          headerShown: false,
           tabBarIcon: ({ color }) => (
             <SymbolView
               name={{
